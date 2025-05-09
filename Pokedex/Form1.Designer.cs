7e225f075fa5edac55d04111eff099f9f84156b1
USUARIOTC <USUARIOTC@DESKTOP-FEN742K>
2025-05-09 14:57:04 -0600

    Agregar archivos de proyecto.

---
+++ Pokedex/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Pokedex
+{
+    partial class Pokedex
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pokedex));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.btLimpiar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(62, 54);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(62, 120);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(39, 16);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(60, 193);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(41, 16);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura";
+            this.lblAltura.Click += new System.EventHandler(this.llbAltura_Click);
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(60, 254);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(35, 16);
+            this.lblTipo.TabIndex = 3;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(121, 328);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(117, 62);
+            this.btBuscar.TabIndex = 4;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // btLimpiar
+            // 
+            this.btLimpiar.Location = new System.Drawing.Point(296, 328);
+            this.btLimpiar.Name = "btLimpiar";
+            this.btLimpiar.Size = new System.Drawing.Size(117, 62);
+            this.btLimpiar.TabIndex = 5;
+            this.btLimpiar.Text = "Limpiar";
+            this.btLimpiar.UseVisualStyleBackColor = true;
+            this.btLimpiar.Click += new System.EventHandler(this.btLimpiar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(146, 51);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(191, 22);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(382, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 151);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Pokedex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 424);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btLimpiar);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Pokedex";
+            this.Text = "Pokedex";
+            this.Load += new System.EventHandler(this.Pokedex_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Button btLimpiar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Pokedex/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+
+namespace Pokedex
+{
+    public partial class Pokedex : Form
+    {
+        private HttpClient client = new HttpClient();
+        public Pokedex()
+        {
+            InitializeComponent();
+        }
+
+        private void llbAltura_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pokedex_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btBuscar_Click(object sender, EventArgs e)
+        {
+            string pokemonName = txtNombre.Text.ToLower();
+            string url = $"https://pokeapi.co/api/v2/pokemon/{pokemonName}";
+
+            try
+            {
+                string response = await client.GetStringAsync(url);
+                JObject json = JObject.Parse(response);
+
+                lblPeso.Text = $"Peso: {json["weight"]}";
+                lblAltura.Text = $"Altura: {json["height"]}";
+                lblTipo.Text = $"Tipo: {json["types"][0]["type"]["name"]}";
+                string imagenUrl = json["sprites"]["front_default"]?.ToString();
+                pictureBox1.Load(imagenUrl);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se encontro la informacion del pokemon: " + ex.Message);
+            }
+        }
+
+        private void btLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = string.Empty;
+
+            lblPeso.Text = "Peso:";
+            lblAltura.Text = "Altura:";
+            lblTipo.Text = "Tipo:";
+
+            pictureBox1.Image = null;
+        }
+    }
+}
